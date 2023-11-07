3cb2529bb47843649494d82e86454d35eac395c9
zewebdev1337 <112774130+zewebdev1337@users.noreply.github.com>
2023-11-07 09:27:55 -0600

    update for dev

--- init-guest.sh
+++ init-guest.sh
@@ -9,7 +9,8 @@ su - admin
 sudo pacman -Syyu
 
 # Install needed packages
-sudo pacman -S spice-vdagent qemu-guest-agent git pacman-contrib zip xdg-user-dirs xfce4-whiskermenu-plugin vlc gimp telegram-desktop gnome-disk-utility baobab galculator p7zip catfish syncthing gpick chromium firefox gparted keepassxc gpa gvfs-smb pcsclite aribb25 aribb24 projectm libgoom2 lirc sdl_image libtiger libkate zvbi lua52-socket libmicrodns protobuf ttf-dejavu smbclient libmtp vcdimager libgme libva-intel-driver libva-vdpau-driver libdc1394 libwmf libopenraw libavif libheif libjxl librsvg webp-pixbuf-loader imagemagick gnome-keyring reflector
+sudo pacman -S spice-vdagent qemu-guest-agent git pacman-contrib zip xdg-user-dirs xfce4-whiskermenu-plugin vlc gimp gnome-disk-utility baobab galculator p7zip catfish syncthing gpick chromium gparted keepassxc gpa gvfs-smb pcsclite aribb25 aribb24 projectm libgoom2 lirc sdl_image libtiger libkate zvbi lua52-socket libmicrodns protobuf ttf-dejavu smbclient libmtp vcdimager libgme libva-intel-driver libva-vdpau-driver libdc1394 libwmf libopenraw libavif libheif libjxl librsvg webp-pixbuf-loader imagemagick gnome-keyring gcc dotnet-sdk gdb clang go json-c xorg-server-devel libxcursor libxrandr libxinerama libxi mingw-w64-gcc docker clang make cmake patch texinfo flex bison gettext wget gsl gmp mpfr libmpc libusb readline libarchive gpgme bash openssl libtool libusb-compat python-pip boost python libvorbis rsync glfw-x11 unzip ninja p7zip gnome-keyring" xdg-user-dir 
+
 # gnome keyring bug all through October 2023: whichever user profile logins first (maybe it's first profile to launch chromium but I lean more towards first login. It's been a pain in the ass, so not a lot of willingness to test further left in me) after first boot is the only one able to use the keyring, further, the accounts that can't use the keyring can't open chromium as it crashes on boot after the GPU error message (this GPU error shows regularly, even when it boots without issues but it logs more stuff past that point when it does. On bugged accounts it stops at the GPU error). Uninstalling gnome-keyring doesn't fix the issue. If gnome-keyring is not installed, all users can boot chromium up
 
 # Install yay
@@ -47,6 +48,7 @@ rm -f "$HOME/.thumbnails/normal/$(echo -n "$4" | md5sum | cut -d " " -f1).png" |
 rm -f "$HOME/.cache/thumbnails/large/$(echo -n "$4" | md5sum | cut -d " " -f1).png" ||\
 rm -f "$HOME/.thumbnails/large/$(echo -n "$4" | md5sum | cut -d " " -f1).png" ||\
 exit 1' >> /usr/bin/folder-thumbnailer
+
 echo '[Thumbnailer Entry]
 Version=1.0
 Encoding=UTF-8
@@ -70,25 +72,16 @@ exit
  
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 xfconf-query -c xsettings -p /Net/ThemeName -s "Adwaita-dark"
-xfconf-query -c xfwm4 -p /general/theme -s Crux
+xfconf-query -c xfwm4 -p /general/theme -s Redmond-XP
 xfconf-query -c xfwm4 -p /general/easy_click -s none
-echo "alias npm='npm '" >> ~/.bashrc
-echo "alias audit='audit --omit=dev'" >> ~/.bashrc
-echo "alias indexupd='react-index-updater & svelte-index-updater & js-index-updater'" >> ~/.bashrc
 #echo "xcape -e 'Super_L=Alt_L|F1'" >> ~/.bashrc
 # TODO: Check if xcape is fixed. it triggers multiple Alt+F1 inputs in quick succession as opposed to a single input as of lately. getting used to using Alt+F1 tho.
 # Enable Syncthing
 systemctl enable --now syncthing.service --user
-# Set and start nvm
-echo 'source /usr/share/nvm/init-nvm.sh' >> ~/.bashrc
-source /usr/share/nvm/init-nvm.sh
-# Install Node.js LTS
-nvm install 20
-# Install index updaters
-npm install --global @zewebdev/react-index-updater @zewebdev/svelte-index-updater @zewebdev/js-index-updater
+
 # Set needed git variables
 git config --global init.defaultBranch "main"
-git config --global user.name "zewebdev1337"
+# git config --global user.name "zewebdev1337"
 # Get email
 keepassxc &
 firefox http://localhost:8384 &
