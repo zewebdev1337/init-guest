7f3813bfa6656be8d06b5acea44478b385f69f2b
zewebdev1337 <112774130+zewebdev1337@users.noreply.github.com>
2023-11-21 04:22:43 -0600

    update init-guest.sh

--- init-guest.sh
+++ init-guest.sh
@@ -6,30 +6,35 @@
 su - admin
  
 # Update
-sudo pacman -Syyu
+sudo dnf upgrade --refresh
 
-# Install needed packages
-sudo pacman -S spice-vdagent qemu-guest-agent git pacman-contrib zip xdg-user-dirs xfce4-whiskermenu-plugin vlc gimp telegram-desktop gnome-disk-utility baobab galculator p7zip catfish syncthing gpick chromium firefox gparted keepassxc gpa gvfs-smb pcsclite aribb25 aribb24 projectm libgoom2 lirc sdl_image libtiger libkate zvbi lua52-socket libmicrodns protobuf ttf-dejavu smbclient libmtp vcdimager libgme libva-intel-driver libva-vdpau-driver libdc1394 libwmf libopenraw libavif libheif libjxl librsvg webp-pixbuf-loader imagemagick reflector docker vultr-cli github-cli
+# Install needed repos
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-## INSTALL AFTER EVERYTHING
-sudo pacman -S gnome-keyring 
-## FIX: LOGIN TO USER, LAUNCH CHROMIUM, INSTALL GNOME-KEYRING, CLOSE AND REOPEN CHROMIUM, SET PASSWORD FOR KEYRING, CLOSE CHROMIUM, REBOOT, PROFIT
+# Repo for VSCode
+sudo dnf install 'dnf-command(config-manager)'
+sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+cat <<EOF | sudo tee /etc/yum.repos.d/vscode.repo
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
 
-# gnome keyring bug all through October 2023: whichever user profile logins first (maybe it's first profile to launch chromium but I lean more towards first login. It's been a pain in the ass, so not a lot of willingness to test further left in me) after first boot is the only one able to use the keyring, further, the accounts that can't use the keyring can't open chromium as it crashes on boot after the GPU error message (this GPU error shows regularly, even when it boots without issues but it logs more stuff past that point when it does. On bugged accounts it stops at the GPU error). Uninstalling gnome-keyring doesn't fix the issue. If gnome-keyring is not installed, all users can boot chromium up
+# Import VSCode repo
+sudo dnf check-update
+sudo dnf install gh code git vlc gimp telegram-desktop docker vultr-cli baobab galculator p7zip syncthing gpick chromium keepassxc samba-client gvfs-smb lirc protobuf vcdimager libva-intel-driver libva-vdpau-driver libopenraw
+# older arch packages, most for vlc sudo dnf install pcsclite aribb25 aribb24 projectm libgoom2 lirc sdl_image libtiger libkate zvbi lua52-socket libmicrodns protobuf ttf-dejavu smbclient libmtp vcdimager libgme libva-intel-driver libva-vdpau-driver libdc1394 libwmf libopenraw libavif libheif libjxl librsvg webp-pixbuf-loader imagemagick reflector docker vultr-cli github-cli
+ 
+# build fails on fedora via dnf
+# dockbarx xfce4-dockbarx-plugin 
 
-# Install yay
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-makepkg -si
-cd ..
-sudo rm -rf ./yay-bin
-
-# Install needed AUR packages
-yay -S visual-studio-code-bin nvm dockbarx xfce4-dockbarx-plugin 
-
-# Clean pacman & yay caches
-sudo paccache -rk0
-sudo rm -rf ~/.cache/yay/*
+# Cleanup
+sudo dnf clean all
 
 exit 
 
@@ -39,6 +44,12 @@ exit
 
 # Switch to root for operations that can only be done as root
 su
+
+# Add non-sudo user
+useradd user -m
+usermod -aG docker user
+passwd user
+
 # Set swappiness
 echo 'vm.swappiness = 200' >> /etc/sysctl.d/99-swappiness.conf
 
@@ -87,11 +98,11 @@ systemctl enable --now syncthing.service --user
 systemctl enable --now docker.service
 # Note that docker.service starts the service on boot, whereas docker.socket starts docker on first usage
 #systemctl enable --now docker.socket
-# Set and start nvm
-echo 'source /usr/share/nvm/init-nvm.sh' >> ~/.bashrc
-source /usr/share/nvm/init-nvm.sh
+# Get and start nvm
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash   
+source ~/.bashrc
 # Install Node.js LTS
-nvm install 20
+nvm install lts/*
 # Install index updaters
 npm install --global @zewebdev/react-index-updater @zewebdev/svelte-index-updater web-ext
 # @zewebdev/js-index-updater 
@@ -104,11 +115,3 @@ firefox http://localhost:8384 &
 firefox https://github.com/settings/emails &
 
 # git config --global user.email "secretgithubemail@users.noreply.github.com"
-
-#########################################################
-#                          UTIL                         #
-#########################################################
-
-# Fix mirrors (pacman/yay slow download)
-# cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bkp
-# sudo reflector --country 'United States' --latest 5 --age 2 --fastest 5 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
